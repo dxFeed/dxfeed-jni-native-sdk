eaa8b0c3e84de87bd6376865df0ea0c8872d04c4
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-04-10 22:35:59 +0300

    Extract buffers to the separate classes

---
+++ src/main/java/com/dxfeed/api/ByteBufferWrapper.java
@@ -0,0 +1,69 @@
+package com.dxfeed.api;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ConcurrentHashMap;
+
+class ByteBufferWrapper {
+    private int pos = 0;
+    private int totalBytes = 0;
+    private final ByteBuffer[] byteChunks;
+    private final ConcurrentHashMap<String, byte[]> cacheStringToBytes;
+    private byte[] data;
+
+    ByteBufferWrapper(int quoteCount, ConcurrentHashMap<String, byte[]> cacheStringToBytes) {
+        byteChunks = new ByteBuffer[quoteCount];
+        this.cacheStringToBytes = cacheStringToBytes;
+    }
+
+    public void addChunk(int idx, int chunkSizeInBytes) {
+        pos = 0;
+        byteChunks[idx] = ByteBuffer.allocate(chunkSizeInBytes);
+        data = byteChunks[idx].array();
+        totalBytes += chunkSizeInBytes;
+    }
+
+    public void writeString(String value) {
+        if (value == null || value.length() == 0) {
+            writeByte((byte) 0); // 0 as empty str
+        } else {
+            byte length = (byte) value.length();
+            writeByte(length);
+            byte[] strBytes = cacheStringToBytes.computeIfAbsent(value, k -> value.getBytes(StandardCharsets.UTF_8));
+            writeBytes(strBytes);
+        }
+    }
+
+    public void writeByte(byte value) {
+        data[pos++] = value;
+    }
+
+    public void writeInt(int value) {
+        data[pos++] = (byte) value;
+        data[pos++] = (byte) (value >> 8);
+        data[pos++] = (byte) (value >> 16);
+        data[pos++] = (byte) (value >> 24);
+    }
+
+    public void writeLong(long value) {
+        for (int i = 0; i < 8; i++) {
+            data[pos++] = (byte) value;
+            value = value >> 8;
+        }
+    }
+
+    public void writeBytes(byte[] value) {
+        System.arraycopy(value, 0, data, pos, value.length);
+        pos += value.length;
+    }
+
+    public byte[] toByteData() {
+        ByteBuffer res = ByteBuffer.allocate(totalBytes);
+        for (ByteBuffer buffer : byteChunks) {
+            res.put(buffer.array());
+        }
+        totalBytes = 0;
+        data = null;
+        return res.array();
+    }
+}
--- src/main/java/com/dxfeed/api/TimeAndSalesNative.java
+++ src/main/java/com/dxfeed/api/TimeAndSalesNative.java
@@ -10,61 +10,13 @@ import java.util.concurrent.ConcurrentHashMap;
 
 class TimeAndSalesNative {
     private static final ConcurrentHashMap<String, byte[]> stringToBytes = new ConcurrentHashMap<>();
-
-    private int totalBytes = 0;
-    private final ByteBuffer[] pBytes;
-    private final DoubleBuffer pDouble;
-
-    private static int writeBytes(byte[] data, int pos, byte[] value) {
-        System.arraycopy(value, 0, data, pos, value.length);
-        return pos + value.length;
-    }
-
-    private static int writeString(byte[] data, int pos, String value) {
-        if (value == null || value.length() == 0) {
-            return writeByte(data, pos, (byte) 0); // 0 as empty str
-        }
-        byte length = (byte) value.length();
-        pos = writeByte(data, pos, length);
-        byte[] strBytes = stringToBytes.computeIfAbsent(value, k -> value.getBytes(StandardCharsets.UTF_8));
-        pos = writeBytes(data, pos, strBytes);
-        return pos;
-    }
-
-    private static int writeByte(byte[] data, int pos, byte value) {
-        data[pos] = value;
-        ++pos;
-        return pos;
-    }
-
-    private static int writeInt(byte[] data, int pos, int value) {
-        data[pos++] = (byte) value;
-        data[pos++] = (byte) (value >> 8);
-        data[pos++] = (byte) (value >> 16);
-        data[pos++] = (byte) (value >> 24);
-        return pos;
-    }
-
-    private static int writeLong(byte[] data, int pos, long value) {
-        for (int i = 0; i < 8; i++) {
-            data[pos++] = (byte) value;
-            value = value >> 8;
-        }
-        return pos;
-    }
-
-    private static int writeDouble(double[] data, int pos, double value) {
-        data[pos++] = value;
-        return pos;
-    }
-
+    private final ByteBufferWrapper pBytes;
+    private final DoubleBufferWrapper pDoubles;
 
     TimeAndSalesNative(List<TimeAndSale> quoteList) {
         int quoteCount = quoteList.size();
-        pBytes = new ByteBuffer[quoteCount];
-        pDouble = DoubleBuffer.allocate(quoteCount * 3);
-        int pos = 0;
-        int doublePos = 0;
+        pBytes = new ByteBufferWrapper(quoteCount, stringToBytes);
+        pDoubles = new DoubleBufferWrapper(quoteCount, 3);
         for (int i = 0; i < quoteCount; ++i) {
             TimeAndSale quote = quoteList.get(i);
             String eventSymbol = quote.getEventSymbol();
@@ -90,41 +42,30 @@ class TimeAndSalesNative {
                 totalSize += (byte) seller.length();
             }
 
-            pBytes[i] = ByteBuffer.allocate(totalSize);
-            ByteBuffer buffer = pBytes[i];
-            final byte[] array = buffer.array();
-            pos = writeString(array, pos, eventSymbol);             // 1 + eventSymbolLength
-            pos = writeLong(array, pos, eventTime);                 // 8
-            pos = writeLong(array, pos, index);                     // 8
-            pos = writeInt(array, pos, event_flags);                // 4
-            pos = writeInt(array, pos, time_nano_part);             // 4
-            pos = writeByte(array, pos, exchange_code);             // 1
-            pos = writeLong(array, pos, quoteSize);                 // 8
-            pos = writeString(array, pos, exchangeSaleConditions);  // 1 + exchangeSaleConditionsLength
-            pos = writeString(array, pos, buyer);                   // 1 +  buyerLength
-            pos = writeString(array, pos, seller);                  // 1 +  sellerLength
-            pos = 0;
-            totalBytes += totalSize;
-
+            pBytes.addChunk(i, totalSize);
+            pBytes.writeString(eventSymbol);             // 1 + eventSymbolLength
+            pBytes.writeLong(eventTime);                 // 8
+            pBytes.writeLong(index);                     // 8
+            pBytes.writeInt(event_flags);                // 4
+            pBytes.writeInt(time_nano_part);             // 4
+            pBytes.writeByte(exchange_code);             // 1
+            pBytes.writeLong(quoteSize);                 // 8
+            pBytes.writeString(exchangeSaleConditions);  // 1 + exchangeSaleConditionsLength
+            pBytes.writeString(buyer);                   // 1 +  buyerLength
+            pBytes.writeString(seller);                  // 1 +  sellerLength
 
             // DOUBLE DATA
-            double[] doubleArray = pDouble.array();
-            doubleArray[doublePos++] = quote.getPrice();
-            doubleArray[doublePos++] = quote.getBidPrice();
-            doubleArray[doublePos++] = quote.getAskPrice();
+            pDoubles.write(quote.getPrice());
+            pDoubles.write(quote.getBidPrice());
+            pDoubles.write(quote.getAskPrice());
         }
     }
 
     public byte[] byteData() {
-        ByteBuffer res = ByteBuffer.allocate(totalBytes);
-        for (ByteBuffer buffer : pBytes) {
-            res.put(buffer.array());
-        }
-        totalBytes = 0;
-        return res.array();
+        return pBytes.toByteData();
     }
 
     public double[] doubleData() {
-        return pDouble.array();
+        return pDoubles.toDoubleData();
     }
 }
