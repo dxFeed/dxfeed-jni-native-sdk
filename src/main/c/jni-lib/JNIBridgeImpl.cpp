f5065d63f9d196348644a1fba3d9ab02b636494f
Andrey Mikhalev <fatum.est.series.causarum@gmail.com>
2023-05-11 00:16:49 +0300

    Working on NativeEventReader

--- src/main/c/jni-lib/JNIBridgeImpl.cpp
+++ src/main/c/jni-lib/JNIBridgeImpl.cpp
@@ -6,44 +6,7 @@
 
 #include "javah/com_dxfeed_api_JniTest.h"
 #include "dxfeed/DxEventListener.hpp"
-
-template <typename T>
-inline T readUByte(char** pData) {
-  T val = (**pData) & 0xFF;
-  ++(*pData);
-  return val;
-}
-
-inline int8_t readByte(char** pData) {
-  return readUByte<int8_t>(pData);
-}
-
-template <typename PrimitiveT>
-inline PrimitiveT readPrimitive(char** pData) {
-  PrimitiveT value = 0;
-  for (size_t n = 0; n < sizeof(PrimitiveT); ++n) {
-    value += readUByte<PrimitiveT>(pData) << (n * 8);
-  }
-  return value;
-}
-
-inline int16_t readInt16_t(char** pData) {
-  return readPrimitive<int16_t>(pData);
-}
-
-inline int32_t readInt(char** pData) {
-  return readPrimitive<int32_t>(pData);
-}
-
-inline int64_t readLong(char** pData) {
-  return readPrimitive<int64_t>(pData);
-}
-
-inline double readDouble(double** pData) {
-  double value = **pData;
-  ++(*pData);
-  return value;
-}
+#include "dxfeed/utils/NativeEventReader.hpp"
 
 #ifdef __cplusplus
 extern "C" {
@@ -74,43 +37,11 @@ void JNICALL Java_com_dxfeed_api_JniTest_nOnQuoteEventListener(JNIEnv* env, jcla
                                                                jbyteArray jEventTypes,
                                                                jlong userCallback)
 {
-  std::vector<dxfg_event_type_t*> events(size);
   auto pByteData = (char*) env->GetPrimitiveArrayCritical(jBytes, nullptr);
   auto pDoubleData = (double*) env->GetPrimitiveArrayCritical(jDoubles, nullptr);
   auto pEventTypes = (char*) env->GetPrimitiveArrayCritical(jEventTypes, nullptr);
 
-  // todo: read event based on pEventType
-  for (int i = 0 ; i < size; ++i) {
-    auto* quote = new dxfg_time_and_sale_t();
-    events[i] = reinterpret_cast<dxfg_event_type_t*>(quote);
-
-    quote->market_event.event_type.clazz = static_cast<dxfg_event_clazz_t>(readByte(&pEventTypes));
-    int16_t strSize = readInt16_t(&pByteData);
-    quote->market_event.event_symbol = pByteData;
-    pByteData += strSize;
-    quote->market_event.event_time = readLong(&pByteData);
-    quote->index = readLong(&pByteData);
-    quote->event_flags = readInt(&pByteData);
-    quote->time_nano_part = readInt(&pByteData);
-    quote->exchange_code = readByte(&pByteData); // todo: check type
-    quote->size = readLong(&pByteData); // todo: check type
-
-    strSize = readInt16_t(&pByteData);
-    quote->exchange_sale_conditions = strSize ? pByteData : "";
-    pByteData += strSize;
-
-    strSize = readInt16_t(&pByteData);
-    quote->buyer = strSize ? pByteData : "";
-    pByteData += strSize;
-
-    strSize = readInt16_t(&pByteData);
-    quote->seller = strSize ? pByteData : "";
-    pByteData += strSize;
-
-    quote->price = readDouble(&pDoubleData);
-    quote->bid_price = readDouble(&pDoubleData);
-    quote->ask_price = readDouble(&pDoubleData);
-  }
+  const auto& events = dxfeed::jni::NativeEventReader::toEvents(size, pByteData, pDoubleData, pEventTypes);
 
   env->ReleasePrimitiveArrayCritical(jDoubles, pDoubleData, 0);
   env->ReleasePrimitiveArrayCritical(jBytes, pByteData, 0);
@@ -130,39 +61,8 @@ void JNICALL JavaCritical_com_dxfeed_api_JniTest_nOnQuoteEventListener(jint size
   auto pByteData = (char*) jBytes;
   auto pDoubleData = (double*) jDoubles;
   auto pEventTypes = (char*) jEventTypes;
-  std::vector<dxfg_event_type_t*> events(size);
 
-  for (int i = 0 ; i < size; ++i) {
-    auto* quote = new dxfg_time_and_sale_t();
-    events[i] = reinterpret_cast<dxfg_event_type_t*>(quote);
-
-    quote->market_event.event_type.clazz = static_cast<dxfg_event_clazz_t>(readByte(&pEventTypes));
-    int16_t strSize = readInt16_t(&pByteData);
-    quote->market_event.event_symbol = pByteData;
-    pByteData += strSize;
-    quote->market_event.event_time = readLong(&pByteData);
-    quote->index = readLong(&pByteData);
-    quote->event_flags = readInt(&pByteData);
-    quote->time_nano_part = readInt(&pByteData);
-    quote->exchange_code = readByte(&pByteData); // todo: check type
-    quote->size = readLong(&pByteData); // todo: check type
-
-    strSize = readInt16_t(&pByteData);
-    quote->exchange_sale_conditions = pByteData;
-    pByteData += strSize;
-
-    strSize = readInt16_t(&pByteData);
-    quote->buyer = strSize ? pByteData : "";
-    pByteData += strSize;
-
-    strSize = readInt16_t(&pByteData);
-    quote->seller = strSize ? pByteData : "";
-    pByteData += strSize;
-
-    quote->price = readDouble(&pDoubleData);
-    quote->bid_price = readDouble(&pDoubleData);
-    quote->ask_price = readDouble(&pDoubleData);
-  }
+  const auto& events = dxfeed::jni::NativeEventReader::toEvents(size, pByteData, pDoubleData, pEventTypes);
 
   auto pListener = reinterpret_cast<dxfeed::DxEventListener*>(userCallback);
   dxfg_event_type_list list = { size, events.data() };
---
+++ src/main/c/jni-lib/include/dxfeed/utils/NativeEventReader.hpp
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MPL-2.0
+
+
+#ifndef DXFEED_JNI_NATIVE_SDK_TIMEANDSALEMAPPER_H_
+#define DXFEED_JNI_NATIVE_SDK_TIMEANDSALEMAPPER_H_
+
+#include "api/dxfg_events.h"
+
+namespace dxfeed::jni {
+  struct NativeEventReader {
+    static std::vector<dxfg_event_type_t*> toEvents(int size, char* pByteData, double* pDoubleData, char* pEventTypes);
+  private:
+    static dxfg_time_and_sale_t* toTimeAndSale(char* pByteData, double* pDoubleData);
+  };
+}
+
+#endif //DXFEED_JNI_NATIVE_SDK_TIMEANDSALEMAPPER_H_
