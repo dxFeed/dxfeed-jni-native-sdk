09b4f71c3006aaf8d09400c3c2e39a87d231dc24
Andrey Mikhalev <fatum.est.series.causarum@gmail.com>
2023-05-16 00:06:22 +0300

    Fix empty property string allocation: initialize char* with zero

--- src/main/c/jni-lib/src/utils/java/JavaLangSystem.cpp
+++ src/main/c/jni-lib/src/utils/java/JavaLangSystem.cpp
@@ -27,7 +27,7 @@ namespace dxfeed::jni {
   const char* JavaLangSystem::getProperty(JNIEnv* env, const char* key) const {
     jstring jKey = env->NewStringUTF(key);
     auto jValue = reinterpret_cast<jstring>(env->CallStaticObjectMethod(javaLangSystemClazz, getPropMethodId, jKey));
-    char* result = new char[0];
+    char* result = new char[1] { 0 };
     if (jValue) {
       auto tmp = env->GetStringUTFChars(jValue, nullptr);
       jsize len = env->GetStringLength(jValue);
