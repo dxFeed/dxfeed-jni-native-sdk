23cc1aa631beadc75dde9f9658e196774cbe6537
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-05-25 23:08:12 +0300

    Working on com.dxfeed.event.market.Quote interop

--- src/main/java/com/dxfeed/api/serializers/QuoteToNative.java
+++ src/main/java/com/dxfeed/api/serializers/QuoteToNative.java
@@ -2,17 +2,64 @@ package com.dxfeed.api.serializers;
 
 import com.dxfeed.api.buffers.ChunkedByteBuffer;
 import com.dxfeed.api.buffers.ChunkedDoubleBuffer;
+import com.dxfeed.event.market.DxFeedEventMarket;
 import com.dxfeed.event.market.Quote;
 
 public class QuoteToNative {
-    public static void convert(Quote quote, ChunkedByteBuffer pBytes, ChunkedDoubleBuffer pDoubles, int eventIdx) {
+    /**
+     * typedef struct dxfg_market_event_t {
+     *     dxfg_event_type_t event_type;
+     *     const char *event_symbol;
+     *     int64_t event_time;
+     * } dxfg_market_event_t;
+     *
+     * typedef struct dxfg_quote_t {
+     *     dxfg_market_event_t market_event;
+     *     int32_t time_millis_sequence;
+     *     int32_t time_nano_part;
+     *     int64_t bid_time;
+     *     int16_t bid_exchange_code;
+     *     double bid_price;
+     *     double bid_size;
+     *     int64_t ask_time;
+     *     int16_t ask_exchange_code;
+     *     double ask_price;
+     *     double ask_size;
+     * } dxfg_quote_t;
+     */
+    public static void convert(Quote event, ChunkedByteBuffer pBytes, ChunkedDoubleBuffer pDoubles, int chunkIdx) {
+        String eventSymbol = event.getEventSymbol();
+        int eventSymbolLength = eventSymbol.length();
+        long eventTime = event.getEventTime();                                                          // 8
+        int timeMillisSequence = DxFeedEventMarket.QuotePackagePrivate.getTimeMillisSequence(event);    // 4
+        int timeNanoPart = event.getTimeNanoPart();                                                     // 4
+        long bidTime = event.getBidTime();                                                              // 8
+        char bidExchangeCode = event.getBidExchangeCode();                                              // 2
+        long bidSize = event.getBidSize();                                                              // 8
+        long askTime = event.getAskTime();                                                              // 8
+        char askExchangeCode = event.getAskExchangeCode();                                              // 2
+        long askSize = event.getAskSize();                                                              // 8
 
-        int totalSize = 0;
+        int totalSize = (2 + eventSymbolLength) + (8) + (4) + (4) + (8) + (2) + (8) + (8) + (2) + (8);
 
-        pBytes.addChunk(eventIdx, totalSize);
+        pBytes.addChunk(chunkIdx, totalSize);
+        pBytes.writeString(eventSymbol);            // 2 + eventSymbolLength
+        pBytes.writeLong(eventTime);                // 8
+        pBytes.writeInt(timeMillisSequence);        // 4
+        pBytes.writeInt(timeNanoPart);              // 4
+        pBytes.writeLong(bidTime);                  // 8
+        pBytes.writeChar(bidExchangeCode);          // 2
+        pBytes.writeLong(bidSize);                  // 8
+        pBytes.writeLong(askTime);                  // 8
+        pBytes.writeChar(askExchangeCode);          // 2
+        pBytes.writeLong(askSize);                  // 8
 
         // DOUBLE DATA
-        pBytes.addChunk(eventIdx, 3);
-
+        double bidPrice = event.getBidPrice();  // 1
+        double askPrice = event.getAskPrice();  // 1
+        // DOUBLE DATA
+        pDoubles.addChunk(chunkIdx, 2);
+        pDoubles.write(bidPrice);
+        pDoubles.write(askPrice);
     }
 }
