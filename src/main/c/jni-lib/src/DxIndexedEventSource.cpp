7855eb6648be97dee874dcc0cf4419e856a57f9a
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-08-08 00:26:48 +0300

    Refactoring DxIndexedEventSource

--- src/main/c/jni-lib/src/DxIndexedEventSource.cpp
+++ src/main/c/jni-lib/src/DxIndexedEventSource.cpp
@@ -10,13 +10,14 @@ namespace dxfeed {
   using namespace jni;
 
   DxIndexedEventSource::DxIndexedEventSource(JNIEnv* env, const char* name) {
-    jclass dxClass = safeFindClass(env, "Lcom/dxfeed/api/DxIndexedEventSource;");
-    jmethodID newOrderedSourceId = safeGetStaticMethodID(env, dxClass, "newOrderSourceByName",
-                                                              "(Ljava/lang/String;[J)Lcom/dxfeed/event/IndexedEventSource;");
+    jclass jDxClass = safeFindClass(env, "Lcom/dxfeed/api/DxIndexedEventSource;");
+    const char* methodName = "newOrderSourceByName";
+    const char* methodSignature = "(Ljava/lang/String;[J)Lcom/dxfeed/event/IndexedEventSource;";
+    jmethodID methodId = safeGetStaticMethodID(env, jDxClass, methodName, methodSignature);
     name_ = name;
     jstring jName = env->NewStringUTF(name);
     jlongArray data = env->NewLongArray(2);
-    indexedEventSource_ = env->CallStaticObjectMethod(dxClass, newOrderedSourceId, jName, data);
+    indexedEventSource_ = env->CallStaticObjectMethod(jDxClass, methodId, jName, data);
     env->DeleteLocalRef(jName);
 
     auto pData = r_cast<jlong*>(env->GetPrimitiveArrayCritical(data, 0));
@@ -24,38 +25,43 @@ namespace dxfeed {
     id_ = static_cast<int32_t>(pData[1]);
     env->ReleasePrimitiveArrayCritical(data, pData, 0);
     env->DeleteLocalRef(data);
+    env->DeleteLocalRef(jDxClass);
   }
 
   DxIndexedEventSource::DxIndexedEventSource(JNIEnv* env, const int32_t sourceId) {
-    jclass dxClass = safeFindClass(env, "Lcom/dxfeed/api/DxIndexedEventSource;");
-    jmethodID newOrderedSourceId = safeGetStaticMethodID(env, dxClass, "newOrderSourceById",
-                                                              "(I[J)Lcom/dxfeed/event/market/OrderSource;");
-
+    jclass jDxClass = safeFindClass(env, "Lcom/dxfeed/api/DxIndexedEventSource;");
+    const char* methodName = "newOrderSourceById";
+    const char* methodSignature = "(I[J)Lcom/dxfeed/event/market/OrderSource;";
+    jmethodID methodId = safeGetStaticMethodID(env, jDxClass, methodName, methodSignature);
     jlongArray data = env->NewLongArray(2);
     indexedEventSource_ = env->NewGlobalRef(
-        env->CallStaticObjectMethod(dxClass, newOrderedSourceId, sourceId, data)
+        env->CallStaticObjectMethod(jDxClass, methodId, sourceId, data)
     );
+    name_ = getName(env);
+    auto jLongData = r_cast<jlong*>(env->GetPrimitiveArrayCritical(data, 0));
+    type_ = static_cast<dxfg_indexed_event_source_type_t>(jLongData[0]);
+    id_ = static_cast<int32_t>(jLongData[1]);
+    env->ReleasePrimitiveArrayCritical(data, jLongData, 0);
+    env->DeleteLocalRef(data);
+    env->DeleteLocalRef(jDxClass);
+  }
 
-    jclass eventSourceClass = env->GetObjectClass(indexedEventSource_);
-    jmethodID nameId = safeGetStaticMethodID(env, eventSourceClass, "name", "()Ljava/lang/String;");
-    auto jName = r_cast<jstring>(env->CallObjectMethod(indexedEventSource_, nameId));
-    env->DeleteLocalRef(eventSourceClass);
+  const char* DxIndexedEventSource::getName(JNIEnv* env) {
+    jclass jEventSourceClass = env->GetObjectClass(indexedEventSource_);
+    const char* methodName = "name";
+    const char* methodSignature = "()Ljava/lang/String;";
+    jmethodID methodId = safeGetMethodID(env, jEventSourceClass, methodName, methodSignature);
+    auto jName = r_cast<jstring>(env->CallObjectMethod(indexedEventSource_, methodId));
 
     jsize stringUtfLength = env->GetStringUTFLength(jName);
     auto name = new char[stringUtfLength + 1];
-    name_ = name;
     name[stringUtfLength] = 0;
     const char* utfChars = env->GetStringUTFChars(jName, 0);
     memcpy(name, utfChars, stringUtfLength);
     env->ReleaseStringUTFChars(jName, utfChars);
     env->DeleteLocalRef(jName);
-
-
-    auto pData = r_cast<jlong*>(env->GetPrimitiveArrayCritical(data, 0));
-    type_ = static_cast<dxfg_indexed_event_source_type_t>(pData[0]);
-    id_ = static_cast<int32_t>(pData[1]);
-    env->ReleasePrimitiveArrayCritical(data, pData, 0);
-    env->DeleteLocalRef(data);
+    env->DeleteLocalRef(jEventSourceClass);
+    return name;
   }
 
   DxIndexedEventSource::~DxIndexedEventSource() {
@@ -75,9 +81,12 @@ namespace dxfeed {
   }
 
   bool DxIndexedEventSource::isSpecialSourceId(JNIEnv* env, int32_t index) {
-    jclass dxClass = safeFindClass(env, "Lcom/dxfeed/event/market/OrderSource;");
-    jmethodID isSpecialSourceIdId = safeGetStaticMethodID(env, dxClass, "isSpecialSourceId", "(I)Z");
-    jboolean result = env->CallStaticBooleanMethod(dxClass, isSpecialSourceIdId, index);
+    jclass jDxClass = safeFindClass(env, "Lcom/dxfeed/event/market/OrderSource;");
+    const char* methodName = "isSpecialSourceId";
+    const char* methodSignature = "(I)Z";
+    jmethodID methodId = safeGetStaticMethodID(env, jDxClass, methodName, methodSignature);
+    jboolean result = env->CallStaticBooleanMethod(jDxClass, methodId, index);
+    env->DeleteLocalRef(jDxClass);
     return result;
   }
 
