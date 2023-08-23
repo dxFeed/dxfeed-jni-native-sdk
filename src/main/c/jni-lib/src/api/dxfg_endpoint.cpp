dcbd815fc3d86a91ab2ba9fb2415f7a8169d879f
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-08-24 00:23:56 +0300

    Working on DxEndpoint API: getInstance2, create2, getState, getRole, closeAndAwaitTermination, user, password, reconnect, disconnect, disconnectAndClear, awaitProcessed, getEventTypes. DxEndpointBuilder API: supportsProperty

--- src/main/c/jni-lib/src/api/dxfg_endpoint.cpp
+++ src/main/c/jni-lib/src/api/dxfg_endpoint.cpp
@@ -56,7 +56,8 @@ int32_t dxfg_DXEndpoint_Builder_withProperties(graal_isolatethread_t* thread, dx
 int32_t dxfg_DXEndpoint_Builder_supportsProperty(graal_isolatethread_t* thread, dxfg_endpoint_builder_t* builder,
                                                  const char* key)
 {
-  return JNI_OK;
+  auto pDxEndpointBuilder = dxfeed::r_cast<dxfeed::DxEndpointBuilder*>(builder);
+  return pDxEndpointBuilder->supportsProperty(thread, key) ? JNI_OK : JNI_ERR;
 }
 
 dxfg_endpoint_t* dxfg_DXEndpoint_Builder_build(graal_isolatethread_t* thread, dxfg_endpoint_builder_t* builder)
@@ -71,8 +72,19 @@ dxfg_endpoint_t* dxfg_DXEndpoint_getInstance(graal_isolatethread_t* thread) {
   return dxfeed::r_cast<dxfg_endpoint_t*>(dxfeed::DxEndpoint::getInstance(thread));
 }
 
+dxfg_endpoint_t* dxfg_DXEndpoint_getInstance2(graal_isolatethread_t* thread, dxfg_endpoint_role_t role) {
+  return dxfeed::r_cast<dxfg_endpoint_t*>(dxfeed::DxEndpoint::getInstance(thread, role));
+}
+
 dxfg_endpoint_t* dxfg_DXEndpoint_create(graal_isolatethread_t* thread) {
   auto pBuilder = std::make_unique<dxfeed::DxEndpointBuilder>(thread);
+  dxfeed::DxEndpoint* pEndpoint = pBuilder->build(thread);
+  return dxfeed::r_cast<dxfg_endpoint_t*>(pEndpoint);
+}
+
+dxfg_endpoint_t* dxfg_DXEndpoint_create2(graal_isolatethread_t* thread, dxfg_endpoint_role_t role) {
+  auto pBuilder = std::make_unique<dxfeed::DxEndpointBuilder>(thread);
+  pBuilder->withRole(thread, role); // todo: check
   dxfeed::DxEndpoint* pEndpoint = pBuilder->build(thread);
   return dxfeed::r_cast<dxfg_endpoint_t*>(pEndpoint);
 }
@@ -89,9 +101,56 @@ int32_t dxfg_DXEndpoint_close(graal_isolatethread_t* thread, dxfg_endpoint_t* en
   return JNI_OK;
 }
 
+int32_t dxfg_DXEndpoint_closeAndAwaitTermination(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  pDxEndpoint->closeAndAwaitTermination(thread);
+  return JNI_OK;
+}
+
+dxfg_endpoint_role_t dxfg_DXEndpoint_getRole(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  return pDxEndpoint->getRole(thread);
+}
+
+int32_t dxfg_DXEndpoint_user(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint, const char* user) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  pDxEndpoint->user(thread, user);
+  return JNI_OK;
+}
+
+int32_t dxfg_DXEndpoint_password(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint, const char* password) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  pDxEndpoint->password(thread, password);
+  return JNI_OK;
+}
+
 int32_t dxfg_DXEndpoint_connect(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint, const char* address) {
   auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
   return pDxEndpoint->connect(thread, address);
+}
+
+int32_t dxfg_DXEndpoint_reconnect(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  pDxEndpoint->reconnect(thread);
+  return JNI_OK;
+}
+
+int32_t dxfg_DXEndpoint_disconnect(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  pDxEndpoint->disconnect(thread);
+  return JNI_OK;
+}
+
+int32_t dxfg_DXEndpoint_disconnectAndClear(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  pDxEndpoint->disconnectAndClear(thread);
+  return JNI_OK;
+}
+
+int32_t dxfg_DXEndpoint_awaitProcessed(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  pDxEndpoint->awaitProcessed(thread);
+  return JNI_OK;
 }
 
 dxfg_feed_t* dxfg_DXEndpoint_getFeed(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
@@ -103,6 +162,11 @@ int32_t dxfg_DXEndpoint_awaitNotConnected(graal_isolatethread_t* thread, dxfg_en
   auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
   pDxEndpoint->awaitNotConnected(thread);
   return JNI_OK;
+}
+
+dxfg_endpoint_state_t dxfg_DXEndpoint_getState(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  return pDxEndpoint->getState(thread);
 }
 
 int32_t dxfg_DXEndpoint_addStateChangeListener(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint,
@@ -121,4 +185,9 @@ int32_t dxfg_DXEndpoint_removeStateChangeListener(graal_isolatethread_t* thread,
   auto* stateChangeListener = dxfeed::r_cast<dxfeed::DxStateChangeListener*>(listener);
   pDxEndpoint->removeStateChangeListener(thread, stateChangeListener);
   return JNI_OK;
+}
+
+dxfg_event_clazz_list_t* dxfg_DXEndpoint_getEventTypes(graal_isolatethread_t* thread, dxfg_endpoint_t* endpoint) {
+  auto* pDxEndpoint = dxfeed::r_cast<dxfeed::DxEndpoint*>(endpoint);
+  return pDxEndpoint->getEventTypes(thread);
 }
--- src/main/java/com/dxfeed/api/DxEndpointJni.java
+++ src/main/java/com/dxfeed/api/DxEndpointJni.java
@@ -1,10 +1,31 @@
 package com.dxfeed.api;
 
+import com.dxfeed.event.EventType;
+
 import java.beans.PropertyChangeListener;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class DxEndpointJni {
   // callbacks from native
+  private static DXEndpoint getInstance(int dxfgEndpointRole) {
+    DXEndpoint.Role role = DXEndpoint.Role.values()[dxfgEndpointRole];
+    return DXEndpoint.getInstance(role);
+  }
+
+  private static DXEndpoint.Builder buildWithRole(DXEndpoint.Builder builder, int dxfgEndpointRole) {
+    DXEndpoint.Role role = DXEndpoint.Role.values()[dxfgEndpointRole];
+    return builder.withRole(role);
+  }
+
+  private static int getRole(DXEndpoint endpoint) {
+    return endpoint.getRole().ordinal();
+  }
+
+  private static int getState(DXEndpoint endpoint) {
+    return endpoint.getState().ordinal();
+  }
+
   private static long newStateChangeEventListener(long userCallback, long userData) {
     long id = DxFeedJni.nextHandleId();
     System.out.println("DxEndpointJni::newStateChangeEventListener, nativeHandle = " + id);
@@ -32,6 +53,17 @@ public class DxEndpointJni {
       System.out.println("DxEndpointJni::removeStateChangeEventListener, nativeHandle = " + nativeHandleId);
       endpoint.removeStateChangeListener(stateChangeListener);
     }
+  }
+
+  private static byte[] getEventTypes(DXEndpoint endpoint) {
+    Set<Class<? extends EventType<?>>> eventTypes = endpoint.getEventTypes();
+    Iterator<Class<? extends EventType<?>>> iterator = eventTypes.iterator();
+    int eventsCount = eventTypes.size();
+    byte[] pEventTypes = new byte[eventsCount];
+    for (int i = 0; i < eventsCount && iterator.hasNext(); ++i) {
+      pEventTypes[i] = DxfgEventClazzT.fromClass(iterator.next());
+    }
+    return pEventTypes;
   }
 
   private static native void nOnStateChangeListener(int oldState, int newState, long userCallback, long userData);
