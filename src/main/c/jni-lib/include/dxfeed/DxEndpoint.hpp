9df407237c5981e0ba1a289f084cb887296cfdd9
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-08-24 00:38:09 +0300

    Working on DxEndpoint API: getInstance2, create2, getState, getRole, closeAndAwaitTermination, user, password, reconnect, disconnect, disconnectAndClear, awaitProcessed, getEventTypes. DxEndpointBuilder API: supportsProperty

--- src/main/c/jni-lib/include/dxfeed/DxEndpoint.hpp
+++ src/main/c/jni-lib/include/dxfeed/DxEndpoint.hpp
@@ -23,15 +23,26 @@ namespace dxfeed {
     DxEndpoint& operator=(const DxEndpoint& other) = delete;
     DxEndpoint& operator=(DxEndpoint&& other) = delete;
 
-    int32_t connect(JNIEnv* env, const char* address);
+    static DxEndpoint* getInstance(JNIEnv* env);
+    static DxEndpoint* getInstance(JNIEnv* env, dxfg_endpoint_role_t dxfgEndpointRole);
+
     DxFeed* getFeed(JNIEnv* env) const;
-    void close(JNIEnv* env) const;
-    void awaitNotConnected(JNIEnv* env) const;
+    dxfg_endpoint_role_t getRole(JNIEnv* pEnv) const;
+    dxfg_endpoint_state_t getState(JNIEnv* pEnv) const;
+    dxfg_event_clazz_list_t* getEventTypes(JNIEnv* env) const;
+
+    int32_t user(JNIEnv* env, const char* userName);
+    int32_t password(JNIEnv* env, const char* password);
+    int32_t connect(JNIEnv* env, const char* address);
+    void reconnect(JNIEnv* env);
+    void disconnect(JNIEnv* env);
+    void disconnectAndClear(JNIEnv* env);
+    void close(JNIEnv* env);
+    void closeAndAwaitTermination(JNIEnv* pEnv);
+    void awaitProcessed(JNIEnv* env);
+    void awaitNotConnected(JNIEnv* env);
     void addStateChangeListener(JNIEnv* env, DxStateChangeListener* listener);
     void removeStateChangeListener(JNIEnv* env, DxStateChangeListener* listener);
-
-    static DxEndpoint* getInstance(JNIEnv* env);
-
   private:
     jobject dxEndpoint_ = nullptr;
     jclass dxEndpointClass_;
