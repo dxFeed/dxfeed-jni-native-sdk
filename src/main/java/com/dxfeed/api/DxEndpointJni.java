97b345af071f35d8bf3f99f84555209e5924da5e
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-09-22 01:08:24 +0300

    Working on Publisher sample

--- src/main/java/com/dxfeed/api/DxEndpointJni.java
+++ src/main/java/com/dxfeed/api/DxEndpointJni.java
@@ -1,5 +1,6 @@
 package com.dxfeed.api;
 
+import com.dxfeed.api.osub.WildcardSymbol;
 import com.dxfeed.event.EventType;
 
 import java.beans.PropertyChangeListener;
@@ -67,4 +68,58 @@ public class DxEndpointJni {
   }
 
   private static native void nOnStateChangeListener(int oldState, int newState, long userCallback, long userData);
+
+  public static void main(String[] args) throws InterruptedException {
+    tapeFile();
+  }
+
+  public static void tapeFile() throws InterruptedException {
+    String inputAddress = "file:ConvertTapeFile.in[readAs=stream_data,speed=max]";
+    String outputAddress = "tape:ConvertTapeFile.out[saveAs=stream_data,format=text]";
+
+    // Create input endpoint configured for tape reading
+    DXEndpoint inputEndpoint = DXEndpoint.newBuilder()
+        .withRole(DXEndpoint.Role.STREAM_FEED) // prevents event conflation and loss due to buffer overflow
+        .withProperty(DXEndpoint.DXFEED_WILDCARD_ENABLE_PROPERTY, "true") // enables wildcard subscription
+        .withProperty(DXEndpoint.DXENDPOINT_EVENT_TIME_PROPERTY, "true") // use provided event times
+        .build();
+    // Create output endpoint configured for tape writing
+    DXEndpoint outputEndpoint = DXEndpoint.newBuilder()
+        .withRole(DXEndpoint.Role.STREAM_PUBLISHER) // prevents event conflation and loss due to buffer overflow
+        .withProperty(DXEndpoint.DXFEED_WILDCARD_ENABLE_PROPERTY, "true") // enables wildcard subscription
+        .withProperty(DXEndpoint.DXENDPOINT_EVENT_TIME_PROPERTY, "true") // use provided event times
+        .build();
+
+    // Create and link event processor for all types of events
+    // Note: set of processed event types could be limited if needed
+    Class<? extends EventType<?>>[] eventTypes = inputEndpoint.getEventTypes().toArray(new Class[inputEndpoint.getEventTypes().size()]);
+    DXFeedSubscription<? extends EventType<?>> sub = inputEndpoint.getFeed().createSubscription(eventTypes);
+    sub.addEventListener(events -> {
+      // Here event processing occurs. Events could be modified, removed, or new events added.
+        /* For example, the below code adds 1 hour to event times:
+        for (EventType event : events)
+            event.setEventTime(event.getEventTime() + 3600_000);
+        */
+      // Publish processed events
+      outputEndpoint.getPublisher().publishEvents(events);
+    });
+    // Subscribe to all symbols
+    // Note: set of processed symbols could be limited if needed
+    sub.addSymbols(WildcardSymbol.ALL);
+
+    // Connect output endpoint and start output tape writing BEFORE starting input tape reading
+    outputEndpoint.connect(outputAddress);
+    // Connect input endpoint and start input tape reading AFTER starting output tape writing
+    inputEndpoint.connect(inputAddress);
+
+    // Wait until all data is read and processed, and then gracefully close input endpoint
+    inputEndpoint.awaitNotConnected();
+    inputEndpoint.closeAndAwaitTermination();
+
+    // Wait until all data is processed and written, and then gracefully close output endpoint
+    outputEndpoint.awaitProcessed();
+    outputEndpoint.closeAndAwaitTermination();
+
+  }
+
 }
