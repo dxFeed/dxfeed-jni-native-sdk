4bf5af4a1115c43d9d732bf335761390666a40c8
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-09-24 19:28:02 +0300

    Fix bytes serialization|deserialization

--- src/main/java/com/dxfeed/api/buffers/ByteBuffer.java
+++ src/main/java/com/dxfeed/api/buffers/ByteBuffer.java
@@ -39,36 +39,38 @@ public class ByteBuffer {
     if (pos + Short.BYTES >= totalSize) {
       resize();
     }
-    data[pos++] = (byte) (value & 0x00FF);
     data[pos++] = (byte) ((value >> Byte.SIZE) & 0xFF);
+    data[pos++] = (byte) (value & 0xFF);
   }
 
   public void writeChar(char value) {
     if (pos + Character.BYTES >= totalSize) {
       resize();
     }
-    data[pos++] = (byte) (value & 0xFF);
     data[pos++] = (byte) ((value >> Byte.SIZE) & 0xFF);
+    data[pos++] = (byte) (value & 0xFF);
   }
 
   public void writeInt(int value) {
     if (pos + Integer.BYTES >= totalSize) {
       resize();
     }
-    for (int i = 0; i < Integer.BYTES; i++) {
-      data[pos++] = (byte) (value & 0xFF);
+    for (int i = Integer.BYTES - 1; i >= 0; --i) {
+      data[pos + i] = (byte) (value & 0xFF);
       value >>= Byte.SIZE;
     }
+    pos += Integer.BYTES;
   }
 
   public void writeLong(long value) {
     if (pos + Long.BYTES >= totalSize) {
       resize();
     }
-    for (int i = 0; i < Long.BYTES; i++) {
-      data[pos++] = (byte) (value & 0xFF);
+    for (int i = Long.BYTES - 1; i >= 0; --i) {
+      data[pos + i] = (byte) (value & 0xFF);
       value >>= Byte.SIZE;
     }
+    pos += Long.BYTES;
   }
 
   public void writeBytes(byte[] value) {
