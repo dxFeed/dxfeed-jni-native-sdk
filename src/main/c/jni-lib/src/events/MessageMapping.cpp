eee42bac6424d3f9b1c7bdb363115090e1965e0c
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2023-12-17 23:00:21 +0300

    Update License info in files

--- src/main/c/jni-lib/src/events/MessageMapping.cpp
+++ src/main/c/jni-lib/src/events/MessageMapping.cpp
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: MPL-2.0
+// Copyright © 2023 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "dxfeed/events/MessageMapping.hpp"
 #include "dxfeed/utils/ByteReader.hpp"
--- src/main/java/com/dxfeed/api/buffers/CString.java
+++ src/main/java/com/dxfeed/api/buffers/CString.java
@@ -1,3 +1,7 @@
+// Copyright © 2023 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 package com.dxfeed.api.buffers;
 
 import java.nio.charset.StandardCharsets;
