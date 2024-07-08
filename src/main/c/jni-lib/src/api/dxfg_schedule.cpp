e3e008cc289243e9dd8611e570d5a33072c2a5d7
Andrei Mikhalev <fatum.est.series.causarum@gmail.com>
2024-07-08 12:31:47 +0300

    Add dxfg_schedule header

---
+++ src/main/c/jni-lib/src/api/dxfg_schedule.cpp
@@ -0,0 +1,17 @@
+// Copyright © 2024 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "api/dxfg_schedule.h"
+
+dxfg_session_filter_t* dxfg_SessionFilter_new(graal_isolatethread_t *thread, dxfg_session_filter_function filter) {
+  // todo: implement
+  return nullptr;
+}
+
+dxfg_session_filter_t* dxfg_SessionFilter_getInstance(graal_isolatethread_t *thread,
+                                                      dxfg_session_filter_prepare_t filter
+) {
+  // todo: implement
+  return nullptr;
+}
